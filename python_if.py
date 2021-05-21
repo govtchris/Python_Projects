902aa653184513bd0a38fc5836538885f060efe3
Chris <79873408+govtchris@users.noreply.github.com>
2021-05-21 17:53:02 -0500

    Continue to learn!

---
+++ python_if.py
@@ -0,0 +1,16 @@
+
+
+num1 = 12
+key = True
+
+if num1 == 12:
+    if key:
+        print("num1 is equal to 12 and they have the key!")
+    else:
+        print("num1 is equal to 12 and THEY DON'T HAVE the key!")
+
+elif num1 < 12:
+    print("num1 is less than 12")
+else:
+    print("num1 isn't equal to 12")
+    
