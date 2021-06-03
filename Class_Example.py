bcaac82e6dfb2d2a297d1c287b8481b7d630bd2f
Chris <79873408+govtchris@users.noreply.github.com>
2021-06-03 12:05:17 -0500

    Update Class_Example.py

--- Class_Example.py
+++ Class_Example.py
@@ -1,21 +1,24 @@
 
 #creating a class for mammals
 class mammal:
+    #using a dunder to let me attach arguments 
     def __init__(self, cName, heatGenerate):
         self.cName = cName
         self.heatGenerate = heatGenerate
 
+    #Creating a print function to let me check to see if the dunder worked
     def myFunction(self):
         print("Hi, I am a " + self.cName + " and I am " + self.heatGenerate)
 
 m1 = mammal("Dog", "endothermic")
 m1.myFunction()
     
-    
+#creating a dog class from the mammal parent class
 class dog(mammal):
     diet = "omnivore"
     title = "Man's Best Friend"
 
+#creating a bird class from the parent mammal class
 class bird(mammal):
     fur = "Feathers, not fur"
     
